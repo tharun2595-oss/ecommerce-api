ca35e3a99ee1bbc3a76fc4e84e0dd6ded1489baf
tharun2595-oss <tharun2595@gmail.com>
2026-04-02 00:51:01 +0530

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-from app.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(String, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
