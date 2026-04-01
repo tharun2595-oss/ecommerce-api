b44264adb4f325427e72ae55e26cdb2d4359d16f
tharun2595-oss <tharun2595@gmail.com>
2026-04-02 00:46:55 +0530

    Update jwt.py

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -1,1 +1,11 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from app.config import SECRET_KEY
 
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
