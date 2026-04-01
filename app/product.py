574fcca8237b7c8a7295b73a8fe2731fcab08dba
tharun2595-oss <tharun2595@gmail.com>
2026-04-02 00:58:47 +0530

    Delete app/product.py

--- app/product.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String, Float
-from app.database import Base
-
-class Product(Base):
-    __tablename__ = "products"
-
-    id = Column(String, primary_key=True, index=True)
-    category = Column(String)
-    price = Column(Float)
