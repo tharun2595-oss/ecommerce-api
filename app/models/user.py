80d07653712691a3f21047436757b7485e69182f
tharun2595-oss <tharun2595@gmail.com>
2026-04-02 01:00:50 +0530

    Add files via upload

---
+++ app/models/user.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, String
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
